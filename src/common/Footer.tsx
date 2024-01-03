31ab2ad15310ee1c7d3fd136fb5a9e36265e8243
dhayaec <dhayaec@gmail.com>
2024-01-03 23:43:51 +0530

    test deploy

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='mt-auto fixed bottom-0 text-center w-full'>
-      <p className='text-slate'>&copy; {new Date().getFullYear()} Stockr App</p>
+      <p className='text-zinc'>&copy; {new Date().getFullYear()} Stockr App</p>
     </footer>
   )
 }
